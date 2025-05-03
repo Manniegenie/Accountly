e5d62cc2eeb30caf956c08b6aeff43caafd1a00b
Manniegenie <Manuelwurld@gmail.com>
2025-05-03 06:06:38 -0700

    changed the schema

--- models/bankbalance.js
+++ models/bankbalance.js
@@ -4,7 +4,11 @@ const bankBalanceSchema = new mongoose.Schema({
   client: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
+    required: false, // Webhook won't always have this, can be added later if needed
+  },
+  monoUserId: {
+    type: String, // From data.user_id in webhook
+    required: false,
   },
   accountId: {
     type: String,
@@ -13,6 +17,10 @@ const bankBalanceSchema = new mongoose.Schema({
   accountNumber: {
     type: String,
     required: true,
+  },
+  name: {
+    type: String, // data.name (e.g., "SAMUEL OLAMIDE NOMO")
+    required: false,
   },
   balance: {
     type: Number,
@@ -26,16 +34,12 @@ const bankBalanceSchema = new mongoose.Schema({
     type: String,
     default: 'Unknown',
   },
-  accountType: {
-    type: String,
-    default: '',
-  },
   fetchedAt: {
     type: Date,
     default: Date.now,
   },
 });
 
-bankBalanceSchema.index({ client: 1, accountId: 1, accountNumber: 1 }, { unique: true });
+bankBalanceSchema.index({ accountId: 1 }, { unique: true });
 
 module.exports = mongoose.model('BankBalanceLog', bankBalanceSchema);
